d9141a1a29fb8b12b7b17b4ff0405a79f076fc0a
masud <masudranaacb@gmail.com>
2024-02-16 21:56:07 +0600

    toggle system added page

---
+++ js/index.js
@@ -0,0 +1,6 @@
+function palyNow() {
+    const homeScren=document.getElementById("home")
+    homeScren.classList.add("hidden")
+    const secontScren=document.getElementById("secont_page")
+    secontScren.classList.remove("hidden")
+}
